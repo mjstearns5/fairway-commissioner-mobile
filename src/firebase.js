8983dda40a79ebfb109450c990678c0dd28973a2
mjstearns5 <mjstearns0824@gmail.com>
2025-12-13 16:39:24 -0500

    Final Firebase Updates

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // PASTE YOUR CONFIG FROM STEP 2 BELOW
 const firebaseConfig = {
-  apiKey: "AIzaSyDLkWCFKFTx_B8O_lUBkvrqbj4osSH6bAI",
-  authDomain: "fairway-commissioner.firebaseapp.com",
-  projectId: "fairway-commissioner",
-  storageBucket: "fairway-commissioner.firebasestorage.app",
-  messagingSenderId: "750462707834",
-  appId: "1:750462707834:web:6befd708a4fbf6e99733c3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
